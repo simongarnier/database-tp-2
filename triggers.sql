0691d861d138194b1538e270a6d01bfdef2f953f
Caroline Pham <phamcaroline@hotmail.com>
2014-12-03 16:45:36 -0500

    Insert triggers
    insertion trigger pour oeuvre limite a la consultation et date de
    retour > date emprunt

--- triggers.sql
+++ triggers.sql
@@ -76,16 +76,72 @@ END nbJours_trigger;
 /
 SHOW ERRORS
 
+-- Verifie que l'oeuvre ne peut pas etre emprunte s'il est limite a la consultation 
+CREATE OR REPLACE TRIGGER consult_trigger
+BEFORE INSERT ON Pret
+REFERENCING OLD AS OLD NEW AS NEW
+FOR EACH ROW
+DECLARE
+CURSOR Curseur5 IS
+	SELECT surPlaceSeulement
+	FROM Exemplaire
+	WHERE Exemplaire.idExemplaire = :NEW.idExemplaire;
+
+consultation INTEGER;
+
+BEGIN
+consultation :=0;
+OPEN Curseur5;
+	FETCH Curseur5 INTO consultation;
+	IF (surPlaceSeulement == 1) THEN
+		raise_application_error(-20100, 'Cet oeuvre ne peut seulement etre en consultation et ne peut pas etre emprunte >>');
+	END IF;
+CLOSE Curseur5;
+END consult_trigger;
+/
+SHOW ERRORS
+
+-- Verifie que la date de retour est plus grand que la date d'emprunt
+CREATE OR REPLACE TRIGGER date_trigger
+BEFORE INSERT ON Pret
+REFERENCING OLD AS OLD NEW AS NEW
+FOR EACH ROW
+DECLARE
+CURSOR Curseur6 IS
+	SELECT dateEmprunt
+	FROM Pret
+	WHERE Pret.idExemplaire = :NEW.idExemplaire;
+
+CURSOR Curseur7 IS
+	SELECT dateRetour
+	FROM Pret
+	WHERE Pret.idExemplaire = :NEW.idExemplaire;
+
+dateEmp DATE;
+dateRet DATE;
+
+BEGIN
+dateEmp :=0;
+OPEN Curseur6;
+	FETCH Curseur6 INTO dateEmp;
+	OPEN Curseur7;
+		FETCH Curseur7 INTO dateRet;
+		IF (dateRet < dateEmp) THEN
+			raise_application_error(-20100, 'La date de retour ne peut pas etre plus petit que la date empruntee');
+		END IF;
+	CLOSE Curseur7;
+CLOSE Curseur6;
+END date_trigger;
+/
+SHOW ERRORS
+
+
 -- On ne peut pas emprunter un oeuvre qui est reserve ou emprunter deja
 -- il faut verifier le nombre disponibles de exemplaires de cet oeuvre
 
 
 -- On ne peut pas reserver un exemplaire qui est deja reserver a ce moment la
 
--- oeuvre limite a la consultation donc peut pas etre emprunter
 
--- la date de retour doit etre plus grand que la date demprunt
-
--- 
 
 SET ECHO OFF
