4ae9f8a01367114456bc992fdd047a5a5f7fac65
Simon Garnier <simon.garnier.g@gmail.com>
2014-12-15 01:15:38 -0500

    Removed old drop

--- drop.sql
+++ drop.sql
@@ -34,16 +34,6 @@ DROP TRIGGER date_trigger;
 DROP TRIGGER emprunt_trigger;
 DROP TRIGGER reserve_trigger;
 
-DROP TRIGGER utilisateur_increment;
-DROP TRIGGER oeuvre_increment;
-DROP TRIGGER emplacement_increment;
-DROP TRIGGER exemplaire_increment;
-DROP TRIGGER reservation_increment;
-DROP TRIGGER pret_increment;
-DROP TRIGGER auteur_increment;
-DROP TRIGGER editeur_increment;
-DROP TRIGGER categorie_increment;
-
 DROP INDEX ind_nom;
 DROP INDEX ind_numero;
 DROP INDEX ind_titre;
